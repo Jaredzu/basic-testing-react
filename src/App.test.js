111cb7d14a0662350aec1dbaeda741f7670e4fa6
Bney28 <bney1@outlook.com>
2023-02-14 15:58:38 -0500

    fix: clean app

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,4 @@
-const { screen } = require("@testing-library/react")
-afterEach(() => {
-	console.log("despues");
-})
-beforeEach(() => {
-	console.log("antes");
-})
+
 describe("App Component", () => {
 // describe == suites test
 // test == test
@@ -13,19 +7,6 @@ describe("App Component", () => {
 		console.log(1)
 	})
 
-	test("App test 2", () => {
-		console.log(2)
-	})
-
 },
 )
 
-describe('App Component 2', () => {
-	test('test', () => {
-		console.log(3);
-	})
-	test('should first', () => {
-		console.log(4);
-	})
-
-});
