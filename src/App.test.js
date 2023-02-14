23730244ecf0847339da4ad49d009017956a3766
arios968 <alejorios40@gmail.com>
2023-02-14 15:23:54 -0500

    Feat: added afterEach,beforeEach

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 const { screen } = require("@testing-library/react")
-
+afterEach(()=>{
+	console.log("despues");
+})
+beforeEach(()=>{
+	console.log("antes");
+})
 describe("App Component", () => {
 
 	test("App test", () => {
-		console.log(screen)
+		console.log(1)
 	})
 
-})
+	test("App test 2", () => {
+		console.log(2)
+	})
+
+},
+)
+
+describe('App Component 2', () => {
+	test('test', () => {
+		console.log(3);
+	})
+	test('should first', () => {
+		console.log(4);
+	})
+
+});
