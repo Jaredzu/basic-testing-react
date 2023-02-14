435b31216e29f61f8b3a7d11e949e5438b77f6da
Jaredzu <jared.zu96@gmail.com>
2023-02-14 17:10:34 -0500

    feat: Add test App: All childs rendered

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,18 @@
+import { render ,screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import App from "./App"
 
 describe("App Component", () => {
 // describe == suites test
 // test == test
 
-	test("App test", () => {
-		console.log(1)
+	test("App-test-1: All childs rendered", () => {
+		render(<App />)
+		const text = screen.getByTestId("text")
+		const btn = screen.getByTestId("button")
+
+		expect(text).toBeInTheDocument()
+		expect(btn).toBeInTheDocument()
 	})
 
 },
