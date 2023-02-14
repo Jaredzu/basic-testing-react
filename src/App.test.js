8194a276dce6596b6275bb557caf99f5c221846c
Jaredzu <jared.zu96@gmail.com>
2023-02-14 17:32:21 -0500

    feat: added App-test-3:Toggle button behavior

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,18 @@
-import { render ,screen } from "@testing-library/react"
+import { cleanup, render, screen, fireEvent } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import App from "./App"
 
+afterEach(() => {
+	cleanup()
+})
+
 describe("App Component", () => {
-// describe == suites test
-// test == test
+	// describe == suites test
+	// test == test
 
 	test("App-test-1: All childs rendered", () => {
+		console.log("App-test-1: All childs rendered");
+
 		render(<App />)
 		const text = screen.getByTestId("text")
 		const btn = screen.getByTestId("button")
@@ -16,11 +22,35 @@ describe("App Component", () => {
 	})
 
 	test("App-test-2: Text component value", () => {
+
+		console.log("App-test-2: Text component value");
 		render(<App />)
 
 		const text = screen.getByTestId("text")
 
 		expect(text).toHaveTextContent("Hello sun!")
+	})
+
+	test("App-test-3:Toggle button behavior", () => {
+		console.log("App-test-3:Toggle button behavior");
+
+		render(<App />)
+
+		const text = screen.getByTestId("text")
+		const btn = screen.getByTestId("button")
+
+
+		expect(text).toHaveTextContent("Hello sun!")
+
+
+		fireEvent.click(btn)
+		expect(text).toBeEmptyDOMElement()
+		
+		fireEvent.click(btn)
+		expect(text).toHaveTextContent("Hello sun!")
+
+		// ... 
+
 	})
 
 },
