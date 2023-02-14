888a069f80f3bf4041a67c2fc486e182a7a44a7c
CaroBte <karito0721@hotmail.com>
2023-02-14 15:23:00 -0500

    feat: added some basic afterEach & beforeEach

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 const { screen } = require("@testing-library/react")
 
+afterEach(() => {
+	console.log("después");
+})
+
+beforeEach(() => {
+	console.log("antes");
+})
+
 describe("App Component", () => {
 
 	test("App test", () => {
-		console.log(screen)
+		console.log(1)
+	})
+	test("App test 2", () => {
+		console.log(2)
 	})
 
+})
+
+describe("App Component 2", () => {
+
+	test("App test 3", () => {
+		console.log(3)
+	})
+	test("App test 4", () => {
+		console.log(4)
+	})
 })
