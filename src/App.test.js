95e7893b43c1adc167d5e22a0f47f6cf25fc5aa4
Marianscabas <mariancabanacabas98@gmail.com>
2023-02-14 15:23:49 -0500

    feat : added afterEach and beforeEach

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 const { screen } = require("@testing-library/react")
 
+afterEach(()=>{
+	console.log("despues");
+});
+
+beforeEach(()=>{
+	console.log("antes");
+});
+
 describe("App Component", () => {
 
 	test("App test", () => {
-		console.log(screen)
+		console.log(1)
+	})
+
+	test("App test", () => {
+		console.log(2)
+	})
+
+})
+
+
+describe("App Component 2", () => {
+
+	test("App test", () => {
+		console.log(3)
+	})
+
+	test("App test", () => {
+		console.log(4)
 	})
 
 })
