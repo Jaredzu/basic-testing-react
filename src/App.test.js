e15759d79252dbb5dad9f892d98fa1aa82b31a32
Bney28 <bney1@outlook.com>
2023-02-14 15:22:32 -0500

    feat: added beforeEach & afterEach

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 const { screen } = require("@testing-library/react")
 
+afterEach(() => {
+	console.log("después");
+})
+
+beforeEach(() => {
+	console.log("antes");
+})
+
+
 describe("App Component", () => {
+// describe == suites test
+// test == test
 
 	test("App test", () => {
-		console.log(screen)
+		console.log(1)
+	})
+
+	test("App test 2", () => {
+		console.log(2)
 	})
 
 })
+
+describe("App Component 2", () => {
+	
+		test("App test", () => {
+			console.log(3)
+		})
+	
+		test("App test 2", () => {
+			console.log(4)
+		})
+	
+	})
