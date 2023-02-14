16cd56bfeac525a972bd660b9117d56454fb1136
Andrés Fernández <andresavena19@gmail.com>
2023-02-14 17:15:54 -0500

    feat: template for Toggle button behavior

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,17 @@
-import { render, screen } from "@testing-library/react"
+import { cleanup, render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import App from "./App"
+
+afterEach(() => {
+	console.log("_____");
+	cleanup()
+})
 
 describe("App Component", () => {
 
 	test("App-test-1: All childs rendered", () => {
+		console.log("App-test-1: All childs rendered\n");
+
 		render(<App />)
 
 		const text = screen.getByTestId("text")
@@ -15,11 +22,30 @@ describe("App Component", () => {
 	})
 
 	test("App-test-2: Text component value", () => {
+		console.log("App-test-2: Text component value\n");
+
+
 		render(<App />)
 
 		const text = screen.getByTestId("text")
 
 		expect(text).toHaveTextContent("Hello sun!")
+	})
+
+
+	test("App-test-3: Toggle button behavior", () => {
+		console.log("App-test-3: Toggle button behavior\n");
+
+
+		render(<App />)
+
+		const text = screen.getByTestId("text")
+		const btn = screen.getByTestId("button")
+
+		expect(text).toHaveTextContent("Hello sun!")
+
+		// ... 
+
 	})
 
 })
